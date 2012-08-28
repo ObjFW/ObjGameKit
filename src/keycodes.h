53b1823dc3bb1749a23c3ca84abe27c5b61d17ce
Jonathan Schleifer <js@webkeks.org>
2012-08-28 19:39:09 +0200

    Add keycodes.h.

---
+++ src/keycodes.h
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2012 Jonathan Schleifer <js@webkeks.org>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the
+ * use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *   1.) The origin of this software must not be misrepresented; you must not
+ *       claim that you wrote the original software. If you use this software
+ *       in a product, an acknowledgment in the product documentation would be
+ *       appreciated but is not required.
+ *   2.) Altered source versions must be plainly marked as such, and must not
+ *       be misrepresented as being the original software.
+ *   3.) This notice may not be removed or altered from any source distribution.
+ */
+
+#define OGK_KEY_A	      ALLEGRO_KEY_A
+#define OGK_KEY_B	      ALLEGRO_KEY_B
+#define OGK_KEY_C	      ALLEGRO_KEY_C
+#define OGK_KEY_D	      ALLEGRO_KEY_D
+#define OGK_KEY_E	      ALLEGRO_KEY_E
+#define OGK_KEY_F	      ALLEGRO_KEY_F
+#define OGK_KEY_G	      ALLEGRO_KEY_G
+#define OGK_KEY_H	      ALLEGRO_KEY_H
+#define OGK_KEY_I	      ALLEGRO_KEY_I
+#define OGK_KEY_J	      ALLEGRO_KEY_J
+#define OGK_KEY_K	      ALLEGRO_KEY_K
+#define OGK_KEY_L	      ALLEGRO_KEY_L
+#define OGK_KEY_M	      ALLEGRO_KEY_M
+#define OGK_KEY_N	      ALLEGRO_KEY_N
+#define OGK_KEY_O	      ALLEGRO_KEY_O
+#define OGK_KEY_P	      ALLEGRO_KEY_P
+#define OGK_KEY_Q	      ALLEGRO_KEY_Q
+#define OGK_KEY_R	      ALLEGRO_KEY_R
+#define OGK_KEY_S	      ALLEGRO_KEY_S
+#define OGK_KEY_T	      ALLEGRO_KEY_T
+#define OGK_KEY_U	      ALLEGRO_KEY_U
+#define OGK_KEY_V	      ALLEGRO_KEY_V
+#define OGK_KEY_W	      ALLEGRO_KEY_W
+#define OGK_KEY_X	      ALLEGRO_KEY_X
+#define OGK_KEY_Y	      ALLEGRO_KEY_Y
+#define OGK_KEY_Z	      ALLEGRO_KEY_Z
+#define OGK_KEY_0	      ALLEGRO_KEY_0
+#define OGK_KEY_1	      ALLEGRO_KEY_1
+#define OGK_KEY_2	      ALLEGRO_KEY_2
+#define OGK_KEY_3	      ALLEGRO_KEY_3
+#define OGK_KEY_4	      ALLEGRO_KEY_4
+#define OGK_KEY_5	      ALLEGRO_KEY_5
+#define OGK_KEY_6	      ALLEGRO_KEY_6
+#define OGK_KEY_7	      ALLEGRO_KEY_7
+#define OGK_KEY_8	      ALLEGRO_KEY_8
+#define OGK_KEY_9	      ALLEGRO_KEY_9
+#define OGK_KEY_PAD_0	      ALLEGRO_KEY_PAD_0
+#define OGK_KEY_PAD_1	      ALLEGRO_KEY_PAD_1
+#define OGK_KEY_PAD_2	      ALLEGRO_KEY_PAD_2
+#define OGK_KEY_PAD_3	      ALLEGRO_KEY_PAD_3
+#define OGK_KEY_PAD_4	      ALLEGRO_KEY_PAD_4
+#define OGK_KEY_PAD_5	      ALLEGRO_KEY_PAD_5
+#define OGK_KEY_PAD_6	      ALLEGRO_KEY_PAD_6
+#define OGK_KEY_PAD_7	      ALLEGRO_KEY_PAD_7
+#define OGK_KEY_PAD_8	      ALLEGRO_KEY_PAD_8
+#define OGK_KEY_PAD_9	      ALLEGRO_KEY_PAD_9
+#define OGK_KEY_F1	      ALLEGRO_KEY_F1
+#define OGK_KEY_F2	      ALLEGRO_KEY_F2
+#define OGK_KEY_F3	      ALLEGRO_KEY_F3
+#define OGK_KEY_F4	      ALLEGRO_KEY_F4
+#define OGK_KEY_F5	      ALLEGRO_KEY_F5
+#define OGK_KEY_F6	      ALLEGRO_KEY_F6
+#define OGK_KEY_F7	      ALLEGRO_KEY_F7
+#define OGK_KEY_F8	      ALLEGRO_KEY_F8
+#define OGK_KEY_F9	      ALLEGRO_KEY_F9
+#define OGK_KEY_F10	      ALLEGRO_KEY_F10
+#define OGK_KEY_F11	      ALLEGRO_KEY_F11
+#define OGK_KEY_F12	      ALLEGRO_KEY_F12
+#define OGK_KEY_ESCAPE	      ALLEGRO_KEY_ESCAPE
+#define OGK_KEY_TILDE	      ALLEGRO_KEY_TILDE
+#define OGK_KEY_MINUS	      ALLEGRO_KEY_MINUS
+#define OGK_KEY_EQUALS	      ALLEGRO_KEY_EQUALS
+#define OGK_KEY_BACKSPACE     ALLEGRO_KEY_BACKSPACE
+#define OGK_KEY_TAB	      ALLEGRO_KEY_TAB
+#define OGK_KEY_OPENBRACE     ALLEGRO_KEY_OPENBRACE
+#define OGK_KEY_CLOSEBRACE    ALLEGRO_KEY_CLOSEBRACE
+#define OGK_KEY_ENTER	      ALLEGRO_KEY_ENTER
+#define OGK_KEY_SEMICOLON     ALLEGRO_KEY_SEMICOLON
+#define OGK_KEY_QUOTE	      ALLEGRO_KEY_QUOTE
+#define OGK_KEY_BACKSLASH     ALLEGRO_KEY_BACKSLASH
+#define OGK_KEY_BACKSLASH2    ALLEGRO_KEY_BACKSLASH2
+#define OGK_KEY_COMMA	      ALLEGRO_KEY_COMMA
+#define OGK_KEY_FULLSTOP      ALLEGRO_KEY_FULLSTOP
+#define OGK_KEY_SLASH	      ALLEGRO_KEY_SLASH
+#define OGK_KEY_SPACE	      ALLEGRO_KEY_SPACE
+#define OGK_KEY_INSERT	      ALLEGRO_KEY_INSERT
+#define OGK_KEY_DELETE	      ALLEGRO_KEY_DELETE
+#define OGK_KEY_HOME	      ALLEGRO_KEY_HOME
+#define OGK_KEY_END	      ALLEGRO_KEY_END
+#define OGK_KEY_PGUP	      ALLEGRO_KEY_PGUP
+#define OGK_KEY_PGDN	      ALLEGRO_KEY_PGDN
+#define OGK_KEY_LEFT	      ALLEGRO_KEY_LEFT
+#define OGK_KEY_RIGHT	      ALLEGRO_KEY_RIGHT
+#define OGK_KEY_UP	      ALLEGRO_KEY_UP
+#define OGK_KEY_DOWN	      ALLEGRO_KEY_DOWN
+#define OGK_KEY_PAD_SLASH     ALLEGRO_KEY_PAD_SLASH
+#define OGK_KEY_PAD_ASTERISK  ALLEGRO_KEY_PAD_ASTERISK
+#define OGK_KEY_PAD_MINUS     ALLEGRO_KEY_PAD_MINUS
+#define OGK_KEY_PAD_PLUS      ALLEGRO_KEY_PAD_PLUS
+#define OGK_KEY_PAD_DELETE    ALLEGRO_KEY_PAD_DELETE
+#define OGK_KEY_PAD_ENTER     ALLEGRO_KEY_PAD_ENTER
+#define OGK_KEY_PRINTSCREEN   ALLEGRO_KEY_PRINTSCREEN
+#define OGK_KEY_PAUSE	      ALLEGRO_KEY_PAUSE
+#define OGK_KEY_ABNT_C1       ALLEGRO_KEY_ABNT_C1
+#define OGK_KEY_YEN	      ALLEGRO_KEY_YEN
+#define OGK_KEY_KANA	      ALLEGRO_KEY_KANA
+#define OGK_KEY_CONVERT       ALLEGRO_KEY_CONVERT
+#define OGK_KEY_NOCONVERT     ALLEGRO_KEY_NOCONVERT
+#define OGK_KEY_AT	      ALLEGRO_KEY_AT
+#define OGK_KEY_CIRCUMFLEX    ALLEGRO_KEY_CIRCUMFLEX
+#define OGK_KEY_COLON2	      ALLEGRO_KEY_COLON2
+#define OGK_KEY_KANJI	      ALLEGRO_KEY_KANJI
+#define OGK_KEY_PAD_EQUALS    ALLEGRO_KEY_PAD_EQUALS
+#define OGK_KEY_BACKQUOTE     ALLEGRO_KEY_BACKQUOTE
+#define OGK_KEY_SEMICOLON2    ALLEGRO_KEY_SEMICOLON2
+#define OGK_KEY_COMMAND       ALLEGRO_KEY_COMMAND
+#define OGK_KEY_UNKNOWN       ALLEGRO_KEY_UNKNOWN
+#define OGK_KEY_MODIFIERS     ALLEGRO_KEY_MODIFIERS
+#define OGK_KEY_LSHIFT	      ALLEGRO_KEY_LSHIFT
+#define OGK_KEY_RSHIFT	      ALLEGRO_KEY_RSHIFT
+#define OGK_KEY_LCTRL	      ALLEGRO_KEY_LCTRL
+#define OGK_KEY_RCTRL	      ALLEGRO_KEY_RCTRL
+#define OGK_KEY_ALT	      ALLEGRO_KEY_ALT
+#define OGK_KEY_ALTGR	      ALLEGRO_KEY_ALTGR
+#define OGK_KEY_LWIN	      ALLEGRO_KEY_LWIN
+#define OGK_KEY_RWIN	      ALLEGRO_KEY_RWIN
+#define OGK_KEY_MENU	      ALLEGRO_KEY_MENU
+#define OGK_KEY_SCROLLLOCK    ALLEGRO_KEY_SCROLLLOCK
+#define OGK_KEY_NUMLOCK       ALLEGRO_KEY_NUMLOCK
+#define OGK_KEY_CAPSLOCK      ALLEGRO_KEY_CAPSLOCK
+#define OGK_KEY_MAX	      ALLEGRO_KEY_MAX
+#define OGK_KEYMOD_SHIFT      ALLEGRO_KEYMOD_SHIFT
+#define OGK_KEYMOD_CTRL       ALLEGRO_KEYMOD_CTRL
+#define OGK_KEYMOD_ALT	      ALLEGRO_KEYMOD_ALT
+#define OGK_KEYMOD_LWIN       ALLEGRO_KEYMOD_LWIN
+#define OGK_KEYMOD_RWIN       ALLEGRO_KEYMOD_RWIN
+#define OGK_KEYMOD_MENU       ALLEGRO_KEYMOD_MENU
+#define OGK_KEYMOD_ALTGR      ALLEGRO_KEYMOD_ALTGR
+#define OGK_KEYMOD_COMMAND    ALLEGRO_KEYMOD_COMMAND
+#define OGK_KEYMOD_SCROLLLOCK ALLEGRO_KEYMOD_SCROLLLOCK
+#define OGK_KEYMOD_NUMLOCK    ALLEGRO_KEYMOD_NUMLOCK
+#define OGK_KEYMOD_CAPSLOCK   ALLEGRO_KEYMOD_CAPSLOCK
+#define OGK_KEYMOD_INALTSEQ   ALLEGRO_KEYMOD_INALTSEQ
+#define OGK_KEYMOD_ACCENT1    ALLEGRO_KEYMOD_ACCENT1
+#define OGK_KEYMOD_ACCENT2    ALLEGRO_KEYMOD_ACCENT2
+#define OGK_KEYMOD_ACCENT3    ALLEGRO_KEYMOD_ACCENT3
+#define OGK_KEYMOD_ACCENT4    ALLEGRO_KEYMOD_ACCENT4
